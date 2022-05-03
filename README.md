5c2db900109dfbb37e0a5dcb58037bec89442d19
Wallace Lin <sjlin0@nuvoton.com>
2022-05-03 10:38:19 +0800

    ASOC: nau8315: upload driver to kernel v3.4 to v5.10
    Signed-off-by: Wallace Lin <sjlin0@nuvoton.com>

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The Nuvoton-ASoC repository stores the Linux driver for codec and amplifier manu
 x:none , o:ok (sync newest version)
 |Kernel      |k3.4   |k3.10  |k3.14  |k3.18  |k3.19  |k4.4-15|k4.18  |k4.19  |k5.10  |note|
 |------------|-------|-------|-------|-------|-------|-------|-------|-------|-------|----------|
-|NAU8315     |x      |x      |x      |x      |x      |x      |x      |x      |x      |upstream at k5.11|
+|NAU8315     |o      |o      |o      |o      |o      |o      |o      |o      |o      |upstream at k5.11|
 |NAU85L40    |o      |o      |o      |o      |o      |o      |o      |o      |o      |upstream at 4.11|
 |NAU88C10    |o      |o      |o      |o      |o      |o      |o      |o      |o      |upstream at 4.9|
 |NAU88L21    |o      |o      |o      |o      |o      |o      |o      |o      |o      |upstream|
---
+++ nau8315/k3.4/nau8315.c
@@ -0,0 +1,158 @@
+/*
+ *
+ * nau8315.c  --  NAU8315 ALSA SoC Audio Amplifier Driver
+ *
+ * Copyright 2020 Nuvoton Technology Crop.
+ *
+ * Author: David Lin <ctlin0@nuvoton.com>
+ *
+ * Based on MAX98357A.c
+ */
+
+#include <linux/device.h>
+#include <linux/err.h>
+#include <linux/gpio.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/of_gpio.h>
+#include <linux/of_device.h>
+#include <linux/platform_device.h>
+#include <sound/pcm.h>
+#include <sound/soc.h>
+#include <sound/soc-dai.h>
+#include <sound/soc-dapm.h>
+
+struct nau8315_priv {
+	int enable;
+	int enpin_switch;
+};
+
+static int nau8315_daiops_trigger(struct snd_pcm_substream *substream,
+		int cmd, struct snd_soc_dai *dai)
+{
+	struct snd_soc_codec *codec = dai->codec;
+	struct nau8315_priv *nau8315 =	snd_soc_codec_get_drvdata(codec);
+
+	if (!nau8315->enable)
+		return 0;
+
+	switch (cmd) {
+	case SNDRV_PCM_TRIGGER_START:
+	case SNDRV_PCM_TRIGGER_RESUME:
+	case SNDRV_PCM_TRIGGER_PAUSE_RELEASE:
+		if (nau8315->enpin_switch) {
+			gpio_set_value(nau8315->enable, 1);
+			dev_dbg(codec->dev, "set enable to 1");
+		}
+		break;
+	case SNDRV_PCM_TRIGGER_STOP:
+	case SNDRV_PCM_TRIGGER_SUSPEND:
+	case SNDRV_PCM_TRIGGER_PAUSE_PUSH:
+		gpio_set_value(nau8315->enable, 0);
+		dev_dbg(codec->dev, "set enable to 0");
+		break;
+	}
+
+	return 0;
+}
+
+static int nau8315_enpin_event(struct snd_soc_dapm_widget *w,
+		struct snd_kcontrol *kcontrol, int event)
+{
+	struct nau8315_priv *nau8315 =
+		snd_soc_codec_get_drvdata(w->codec);
+
+	if (event & SND_SOC_DAPM_PRE_PMU)
+		nau8315->enpin_switch = 1;
+	else if (event & SND_SOC_DAPM_POST_PMD)
+		nau8315->enpin_switch = 0;
+
+	return 0;
+}
+
+static const struct snd_soc_dapm_widget nau8315_dapm_widgets[] = {
+	SND_SOC_DAPM_OUTPUT("Speaker"),
+	SND_SOC_DAPM_OUT_DRV_E("EN_Pin", SND_SOC_NOPM, 0, 0, NULL, 0,
+			nau8315_enpin_event,
+			SND_SOC_DAPM_PRE_PMU | SND_SOC_DAPM_POST_PMD),
+};
+
+static const struct snd_soc_dapm_route nau8315_dapm_routes[] = {
+	{"EN_Pin", NULL, "HiFi Playback"},
+	{"Speaker", NULL, "EN_Pin"},
+};
+
+static const struct snd_soc_codec_driver nau8315_codec_driver = {
+	.dapm_widgets		= nau8315_dapm_widgets,
+	.num_dapm_widgets	= ARRAY_SIZE(nau8315_dapm_widgets),
+	.dapm_routes		= nau8315_dapm_routes,
+	.num_dapm_routes	= ARRAY_SIZE(nau8315_dapm_routes),
+};
+
+static const struct snd_soc_dai_ops nau8315_dai_ops = {
+	.trigger	= nau8315_daiops_trigger,
+};
+
+#define NAU8315_RATES SNDRV_PCM_RATE_8000_96000
+#define NAU8315_FORMATS (SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S24_3LE)
+
+static struct snd_soc_dai_driver nau8315_dai_driver = {
+	.name = "nau8315-hifi",
+	.playback = {
+		.stream_name	= "HiFi Playback",
+		.formats	= NAU8315_FORMATS,
+		.rates		= NAU8315_RATES,
+		.channels_min	= 1,
+		.channels_max	= 2,
+	},
+	.ops    = &nau8315_dai_ops,
+};
+
+static int nau8315_platform_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct nau8315_priv *nau8315;
+
+	nau8315 = devm_kzalloc(dev, sizeof(*nau8315), GFP_KERNEL);
+	if (!nau8315)
+		return -ENOMEM;
+
+	nau8315->enable = -EINVAL;
+
+	if (of_match_device(of_match_ptr(nau8315_device_id), dev))
+		nau8315->enable = of_get_named_gpio(dev->of_node,
+							"enable-gpio", 0);
+
+	if (gpio_is_valid(nau8315->enable))
+		if (devm_gpio_request_one(dev, nau8315->enable,
+				GPIOF_OUT_INIT_LOW, "enable"))
+			nau8315->enable = -EINVAL;
+
+	dev_set_drvdata(&pdev->dev, nau8315);
+
+	return snd_soc_register_codec(&pdev->dev,
+			&nau8315_codec_driver,
+			&nau8315_dai_driver, 1);
+}
+
+#ifdef CONFIG_OF
+static const struct of_device_id nau8315_device_id[] = {
+	{ .compatible = "nuvoton,nau8315" },
+	{}
+};
+MODULE_DEVICE_TABLE(of, nau8315_device_id);
+#endif
+
+static struct platform_driver nau8315_platform_driver = {
+	.driver = {
+		.name = "nau8315",
+		.of_match_table = of_match_ptr(nau8315_device_id),
+	},
+	.probe	= nau8315_platform_probe,
+};
+module_platform_driver(nau8315_platform_driver);
+
+MODULE_DESCRIPTION("ASoC NAU8315 Mono Class-D Amplifier Driver");
+MODULE_AUTHOR("David Lin <ctlin0@nuvoton.com>");
+MODULE_LICENSE("GPL v2");
